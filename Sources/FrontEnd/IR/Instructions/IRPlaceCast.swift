e077f7b73796aefe21857764a5654252150d16a0
Dimi Racordon <dimi.racordon@gmail.com>
2026-04-04 16:44:20 +0200

    Add instructions to support existentialization

---
+++ Sources/FrontEnd/IR/Instructions/IRPlaceCast.swift
@@ -0,0 +1,50 @@
+/// Converts a place of type `A` to a place of type `B`.
+///
+/// The conversion is not checked. Accessing the resulting place has undefined behavior unless `B`
+/// is layout-compatible with `A`.
+public struct IRPlaceCast: Instruction {
+
+  /// The operands of the instruction.
+  public let operands: [IRValue]
+
+  /// The region of the code corresponding to this instruction.
+  public let anchor: Anchor
+
+  /// The type of the resulting place.
+  public let target: AnyTypeIdentity
+
+  /// Creates an instance with the given properties.
+  public init(source: IRValue, target: AnyTypeIdentity, anchor: Anchor) {
+    self.operands = [source]
+    self.anchor = anchor
+    self.target = target
+  }
+
+  /// Creates a copy of `other`, substituting its properities with `ss`.
+  public init(_ other: Self, substituting ss: IRSubstitutionTable) {
+    self.operands = [ss[other.source]]
+    self.anchor = other.anchor
+    self.target = other.target
+  }
+
+
+  /// The place being converted.
+  public var source: IRValue {
+    operands[0]
+  }
+
+  /// The type of the instruction's result.
+  public var type: IRType {
+    .lowered(target, isAddress: true)
+  }
+
+}
+
+extension IRPlaceCast: Showable {
+
+  /// Returns a textual representation of `self` using `printer`.
+  public func show(using printer: inout TreePrinter) -> String {
+    "place_cast \(printer.show(source)) to \(printer.show(target))"
+  }
+
+}
---
+++ Sources/FrontEnd/IR/Instructions/IRTypeWitness.swift
@@ -0,0 +1,50 @@
+/// Creates a type witness using a runtime type constructor.
+public struct IRTypeWitness: Instruction {
+
+  /// The operands of the instruction.
+  public let operands: [IRValue]
+
+  /// The region of the code corresponding to this instruction.
+  public let anchor: Anchor
+
+  /// An existential type describing the type witness being constructed.
+  public var constructor: UniversalType.ID
+
+  /// The type of the type witness being constructed.
+  public let typeOfApplication: TypeWitness.ID
+
+  /// Creates an instance with the given properties.
+  public init(
+    constructor: UniversalType.ID, arguments: [IRValue], typeOfApplication: TypeWitness.ID,
+    anchor: Anchor
+  ) {
+    self.operands = arguments
+    self.anchor = anchor
+    self.constructor = constructor
+    self.typeOfApplication = typeOfApplication
+  }
+
+  /// Creates a copy of `other`, substituting its properities with `ss`.
+  public init(_ other: Self, substituting ss: IRSubstitutionTable) {
+    self.operands = other.operands.map({ (o) in ss[o] })
+    self.anchor = other.anchor
+    self.constructor = other.constructor
+    self.typeOfApplication = other.typeOfApplication
+  }
+
+  /// The type of the instruction's result.
+  public var type: IRType {
+    .lowered(typeOfApplication.erased, isAddress: true)
+  }
+
+}
+
+extension IRTypeWitness: Showable {
+
+  /// Returns a textual representation of `self` using `printer`.
+  public func show(using printer: inout TreePrinter) -> String {
+    let xs = operands.map({ (o) in printer.show(o) }).joined(separator: ", ")
+    return "type_witness \(printer.show(constructor))(\(xs))"
+  }
+
+}
