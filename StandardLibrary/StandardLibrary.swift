fd44ab64a7a2823d6601e2d1b804f2dbf03c15b0
Ambrus Tóth <32463042+tothambrus11@users.noreply.github.com>
2026-04-07 10:06:59 +0300

    Build hc distributable (#75)
    * Expose standard library resources
    
    * Pin Windows SDK version
    
    * Add distributable building workflow for releases
    
    * Use local standard library by default, add compile flag to use bundled version in distributable builds
    
    * Use consistent archive names for releases corresponding to LSP
    
    * Update StandardLibrary/StandardLibrary.swift
    
    Co-authored-by: Dimi Racordon <dimi.racordon@gmail.com>
    
    * Update StandardLibrary/StandardLibrary.swift
    
    Co-authored-by: Dimi Racordon <dimi.racordon@gmail.com>
    
    * Update Tests/CompilerTests/StandardLibraryLoadingTests.swift
    
    Co-authored-by: Dimi Racordon <dimi.racordon@gmail.com>
    
    * Update Tests/CompilerTests/StandardLibraryLoadingTests.swift
    
    Co-authored-by: Dimi Racordon <dimi.racordon@gmail.com>
    
    ---------
    
    Co-authored-by: Dimi Racordon <dimi.racordon@gmail.com>

--- StandardLibrary/StandardLibrary.swift
+++ StandardLibrary/StandardLibrary.swift
@@ -1,6 +1,15 @@
 import Foundation
 
-/// The root folder of the standard library's sources.
-public let standardLibrarySources = URL(fileURLWithPath: #filePath)
+/// The root folder of the standard library's sources. 
+/// 
+/// This folder should be preferred during development. It is the Driver's default unless the
+/// flag `USE_BUNDLED_STANDARD_LIBRARY` is set.
+public let localStandardLibrarySources = URL(fileURLWithPath: #filePath)
   .deletingLastPathComponent()
   .appendingPathComponent("Sources")
+
+/// The path to the bundled standard library's root folder.
+/// 
+/// This folder is meant to be used in distributable builds in order to bundle the standard library
+/// together with the executable. Set the flag `USE_BUNDLED_STANDARD_LIBRARY` to select it.
+public let bundledStandardLibrarySources = Bundle.module.url(forResource: "Sources", withExtension: nil)!
---
+++ Tests/CompilerTests/StandardLibraryLoadingTests.swift
@@ -0,0 +1,23 @@
+import FrontEnd
+import XCTest
+import Driver
+import StandardLibrary
+
+final class StandardLibraryLoadingTests: XCTestCase {
+
+  func testStandardLibraryLoading() async throws {
+    var driver = Driver()
+    try await driver.loadStandardLibrary()
+  }
+
+  func testStandardLibraryLoadingBundled() async throws {
+    var driver = Driver()
+    try await driver.load(Module.standardLibraryName, withSourcesAt: bundledStandardLibrarySources)
+  }
+
+  func testStandardLibraryLoadingLocal() async throws {
+    var driver = Driver()
+    try await driver.load(Module.standardLibraryName, withSourcesAt: localStandardLibrarySources)
+  }
+
+}
