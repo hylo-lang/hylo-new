82f9547cfce07c5162ee0629a3b2ddc10d3c978d
Ambrus Tóth <32463042+tothambrus11@users.noreply.github.com>
2026-04-07 09:56:00 +0300

    Update StandardLibrary/StandardLibrary.swift
    Co-authored-by: Dimi Racordon <dimi.racordon@gmail.com>

--- StandardLibrary/StandardLibrary.swift
+++ StandardLibrary/StandardLibrary.swift
@@ -10,5 +10,6 @@ public let localStandardLibrarySources = URL(fileURLWithPath: #filePath)
 
 /// The path to the bundled standard library's root folder.
 /// 
-/// Use this in distributable builds. Set compiler flag USE_BUNDLED_STANDARD_LIBRARY to true for Driver to use this.
+/// This folder is meant to be used in distributable builds in order to bundle the standard library
+/// together with the executable. Set the flag `USE_BUNDLED_STANDARD_LIBRARY` to select it.
 public let bundledStandardLibrarySources = Bundle.module.url(forResource: "Sources", withExtension: nil)!
