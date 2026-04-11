dec204b4fd037e2e8a4fc9de5539d2833859fd1e
Dimi Racordon <dimi.racordon@gmail.com>
2026-04-11 08:18:10 +0200

    Remove dead code

--- Sources/FrontEnd/Syntax/DeclarationReference.swift
+++ Sources/FrontEnd/Syntax/DeclarationReference.swift
@@ -74,15 +74,6 @@ public enum DeclarationReference: Hashable, Sendable {
     }
   }
 
-  /// Returns a copy of `self` in which occurrences of `m` have been substituted for `n`.
-  internal func substituting(_ m: ExpressionIdentity, for n: ExpressionIdentity) -> Self {
-    if case .inherited(let w, let d, let s) = self {
-      return .inherited(w.substituting(m, with: n), d, statically: s)
-    } else {
-      return self
-    }
-  }
-
 }
 
 extension DeclarationReference: Showable {
