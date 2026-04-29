19120a27a0c60e19a48f3eb81c604c6c4674f37c
Dimi Racordon <dimi.racordon@gmail.com>
2026-04-29 22:35:13 +0200

    Fix the lowering of calls to members declared in extensions

--- Sources/FrontEnd/IR/IREmitter.swift
+++ Sources/FrontEnd/IR/IREmitter.swift
@@ -779,6 +779,11 @@ internal struct IREmitter {
       .init(value: value, arguments: Array(arguments, terminatedBy: a), result: result)
     }
 
+    /// Returns `self` substituting `v` for `self.value`.
+    consuming func substituting(value v: IRValue) -> LoweredCallee {
+      .init(value: v, arguments: arguments, result: result)
+    }
+
   }
 
   /// Generates the IR for using `e`, which occurs as a callee.
@@ -833,12 +838,22 @@ internal struct IREmitter {
 
     case .direct(let d):
       // The callee refers to a function directly.
-      return loweredCallee(referringTo: d, boundTo: nil, output: result, at: anchor)
+      return loweredCallee(referringTo: d, boundTo: nil, output: result)
 
     case .member(let d):
       // The callee refers to a bound member.
       let q = receiver(&self)
-      return loweredCallee(referringTo: d, boundTo: q, output: result, at: anchor)
+      let f = loweredCallee(referringTo: d, boundTo: q, output: result)
+
+      // If the reference is bound to a generic type, its type arguments have to be extracted from
+      // the receiver's expression.
+      let r = currentFunction.result(of: q)!.type
+      if let ts = program.types.select(r, \TypeApplication.arguments) {
+        let g = lowering(at: anchor.site, in: anchor.scope, { $0._type_apply(f.value, to: ts) })
+        return f.substituting(value: g)
+      } else {
+        return f
+      }
 
     case .inherited(let w, let m, let statically):
       // The callee refers to a member declared in extension.
@@ -846,7 +861,7 @@ internal struct IREmitter {
 
       // Is the member declared in an extension?
       if program.isExtensionMember(m) {
-        let target = loweredCallee(referringTo: m, boundTo: q, output: result, at: anchor)
+        let target = loweredCallee(referringTo: m, boundTo: q, output: result)
         return lowering(at: anchor.site, in: anchor.scope) { (me) in
           let (ts, xs) = me._emitArguments(of: w)
           let f = ts.isEmpty ? target.value : me._type_apply(target.value, to: ts)
@@ -874,18 +889,17 @@ internal struct IREmitter {
 
   /// Generates the IR for a callee referring to `d`, optionally bound to `receiver`.
   private mutating func loweredCallee(
-    referringTo d: DeclarationIdentity, boundTo receiver: IRValue?, output result: IRValue,
-    at anchor: Anchor
+    referringTo d: DeclarationIdentity, boundTo receiver: IRValue?, output result: IRValue
   ) -> LoweredCallee {
     switch program.tag(of: d) {
     case FunctionDeclaration.self:
       let f = demandLoweredDeclaration(functionOrConformance: d)
-      return loweredCallee(referringTo: f, boundTo: receiver, output: result, at: anchor)
+      return loweredCallee(referringTo: f, boundTo: receiver, output: result)
 
     case EnumCaseDeclaration.self:
       let f = demandLoweredDeclaration(
         constructor: program.castUnchecked(d, to: EnumCaseDeclaration.self))
-      return loweredCallee(referringTo: f, boundTo: receiver, output: result, at: anchor)
+      return loweredCallee(referringTo: f, boundTo: receiver, output: result)
 
     default:
       program.unexpected(d)
@@ -894,20 +908,9 @@ internal struct IREmitter {
 
   /// Generates the IR for a callee referring to `function`, optionally bound to `receiver`.
   private mutating func loweredCallee(
-    referringTo function: IRFunction.ID, boundTo receiver: IRValue?, output result: IRValue,
-    at anchor: Anchor
+    referringTo function: IRFunction.ID, boundTo receiver: IRValue?, output result: IRValue
   ) -> LoweredCallee {
-    var v = functionReference(to: function)
-
-    // If the reference is bound to a generic type, its type arguments have to be extracted from
-    // the receiver's expression.
-    if let r = receiver {
-      let q = currentFunction.result(of: r)!.type
-      if let ts = program.types.select(q, \TypeApplication.arguments) {
-        v = lowering(at: anchor.site, in: anchor.scope, { $0._type_apply(v, to: ts) })
-      }
-    }
-
+    let v = functionReference(to: function)
     return LoweredCallee(value: v, arguments: Array(contentsOf: receiver), result: result)
   }
 
@@ -942,7 +945,7 @@ internal struct IREmitter {
       }))
 
     let mono = lowering(e, { (me) in me._type_apply(poly.value, to: a) })
-    return LoweredCallee(value: mono, arguments: poly.arguments, result: poly.result)
+    return poly.substituting(value: mono)
   }
 
   /// Implements `loweredCallee(_:writingTo)` for synthetic expressions.
@@ -971,7 +974,7 @@ internal struct IREmitter {
     case .typeApplication(let f, let a):
       let poly = loweredCallee(f, output: result, at: site, in: scope)
       let mono = lowering(at: site, in: scope, { (me) in me._type_apply(poly.value, to: a) })
-      return LoweredCallee(value: mono, arguments: poly.arguments, result: poly.result)
+      return poly.substituting(value: mono)
 
     default:
       fatalError("not implemented")
