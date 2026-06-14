e6b00dbf379bd6037f2bd753b23e2cd1490810ac
Dimi Racordon <dimi.racordon@gmail.com>
2026-06-14 11:49:18 +0200

    Improve the API around the typing of numeric literals

--- Sources/FrontEnd/Syntax/Expressions/LiteralExpression.swift
+++ Sources/FrontEnd/Syntax/Expressions/LiteralExpression.swift
@@ -4,4 +4,10 @@ public protocol LiteralExpression: Expression {
   /// The literal type associated with `Self`.
   static var literalType: LiteralType { get }
 
+  /// The inferred type of a literal expression in the absence of external constraints.
+  static var defaultType: Program.StandardLibraryEntity { get }
+
+  /// The label of the constructor accepting a literal expression.
+  static var constructorLabel: String { get }
+
 }
