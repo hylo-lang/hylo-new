85bc63d0459ec0a73533860460197fe5f6433661
Dimi Racordon <dimi.racordon@gmail.com>
2026-06-19 10:04:16 +0200

    Implement 128-bit hashing

--- Tests/UtilitiesTests/HashingTests.swift
+++ Tests/UtilitiesTests/HashingTests.swift
@@ -4,11 +4,11 @@ import XCTest
 final class HashingTests: XCTestCase {
 
   func testCombineInt() {
-    var h1 = FNV()
+    var h1 = FNV1.native()
     h1.combine(42)
     h1.combine(1337)
 
-    var h2 = FNV()
+    var h2 = FNV1.native()
     h2.combine(42)
     h2.combine(1337)
     XCTAssertEqual(h1.state, h2.state)
@@ -18,16 +18,28 @@ final class HashingTests: XCTestCase {
   }
 
   func testCombineString() {
-    var h1 = FNV()
+    var h1 = FNV1.native()
     h1.combine("a")
     h1.combine("bcd")
 
-    var h2 = FNV()
+    var h2 = FNV1.native()
     h2.combine("a")
     h2.combine("bcd")
     XCTAssertEqual(h1.state, h2.state)
 
     h2.combine("xy")
+    XCTAssertNotEqual(h1.state, h2.state)
+  }
+
+  func testU128() {
+    var h1 = FNV1.u128()
+    h1.combine(bytes: 0 ..< 10)
+
+    var h2 = FNV1.u128()
+    h2.combine(bytes: 0 ..< 10)
+    XCTAssertEqual(h1.state, h2.state)
+
+    h1.combine(11)
     XCTAssertNotEqual(h1.state, h2.state)
   }
 
