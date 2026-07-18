13c5a2315c3978026b0302525edadb566d0e50a2
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2026-07-18 13:56:53 +0530

    added concrete type

---
+++ Sources/Interpreter/ConcreteType.swift
@@ -0,0 +1,16 @@
+import FrontEnd
+
+/// The identity of a concrete (non-generic) type.
+struct ConcreteTypeIdentity {
+
+  /// The underlying type identity, guaranteed to have no generic parameters.
+  public let underlying: AnyTypeIdentity
+
+  /// Creates an instance wrapping `t` as `ConcreteTypeIdentity`.
+  ///
+  /// - Precondition: `t` doesn't have any generic parameter.
+  public init(_ t: AnyTypeIdentity) {
+    underlying = t
+  }
+
+}
