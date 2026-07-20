9f133dbfdae6d158a5adab0d3dcfeef138d18fd5
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2026-07-20 21:13:49 +0530

    pass program as inout

--- Sources/Interpreter/TypeLayoutCache.swift
+++ Sources/Interpreter/TypeLayoutCache.swift
@@ -4,52 +4,55 @@ import Utilities
 /// A memoizing computer of type layouts.
 struct TypeLayoutCache {
 
-  /// The program supplying the types.
-  var p: Program
-
   /// The ABI for which the types will be laid out.
   let abi: any TargetABI
 
   /// The memo of layouts computed so far.
   private var storage: [MonomorphicTypeIdentity: TypeLayout] = [:]
 
-  /// An instance for laying out types in `p` according to `abi`.
-  public init(typesIn p: Program, for abi: any TargetABI) {
-    self.p = p
+  /// An instance for laying out types according to `abi`.
+  public init(for abi: any TargetABI) {
     self.abi = abi
   }
 
-  /// The layout for `t`.
-  public subscript(_ t: MonomorphicTypeIdentity) -> TypeLayout {
-    mutating get {
-      if let r = storage[t] { return r }
-      let k = MonomorphicTypeIdentity(p.types.dealiased(t.underlying))
-      let r = computeLayout(k)
-      storage[t] = r
-      return r
-    }
+  /// The layout for `t` in `p`.
+  public mutating func layout(
+    _ t: MonomorphicTypeIdentity,
+    in p: inout Program
+  ) -> TypeLayout {
+    if let r = storage[t] { return r }
+    let k = MonomorphicTypeIdentity(p.types.dealiased(t.underlying))
+    let r = computeLayout(k, in: &p)
+    storage[t] = r
+    return r
   }
 
-  /// Returns the layout for `t`.
+  /// Returns the layout for `t` in `p`.
   ///
   /// - Precondition: `t` is not an alias.
-  private mutating func computeLayout(_ t: MonomorphicTypeIdentity) -> TypeLayout {
-    if isMachineType(t.underlying) {
-      let u = type(t.underlying, as: MachineType.self)
+  private mutating func computeLayout(
+    _ t: MonomorphicTypeIdentity,
+    in p: inout Program
+  ) -> TypeLayout {
+    if isMachineType(t.underlying, in: p) {
+      let u = type(t.underlying, as: MachineType.self, in: p)
       return TypeLayout(bytes: abi.layout(u), type: t, parts: [], isEnumLayout: false)
-    } else if isEnum(t.underlying) {
+    } else if isEnum(t.underlying, in: p) {
       unimplemented()
-    } else if isStruct(t.underlying) {
-      return computeLayout(struct: t)
-    } else if isTuple(t.underlying) {
-      return computeLayout(tuple: t)
+    } else if isStruct(t.underlying, in: p) {
+      return computeLayout(struct: t, in: &p)
+    } else if isTuple(t.underlying, in: p) {
+      return computeLayout(tuple: t, in: &p)
     } else {
       unreachable("\(p.show(t.underlying)) doesn't have any layout)")
     }
   }
 
-  /// Returns the layout for struct `t`.
-  private mutating func computeLayout(struct t: MonomorphicTypeIdentity) -> TypeLayout {
+  /// Returns the layout for struct `t` in `p`.
+  private mutating func computeLayout(
+    struct t: MonomorphicTypeIdentity,
+    in p: inout Program
+  ) -> TypeLayout {
     let d = p.declaration(of: t.underlying)!
     let m = p.parent(containing: d).module
     let ms = p.storage(of: t.underlying, visibleFrom: m)!
@@ -57,11 +60,16 @@ struct TypeLayoutCache {
       record: t,
       havingMembers: ms.map {
         (type: MonomorphicTypeIdentity($0), label: nil)
-      })
+      },
+      in: &p
+    )
   }
 
-  /// Returns the layout for tuple `t`.
-  private mutating func computeLayout(tuple t: MonomorphicTypeIdentity) -> TypeLayout {
+  /// Returns the layout for tuple `t` in `p`.
+  private mutating func computeLayout(
+    tuple t: MonomorphicTypeIdentity,
+    in p: inout Program
+  ) -> TypeLayout {
     let u = ConcreteTypeIdentity<Tuple>(uncheckedFrom: t.underlying)
     let (ms, o) = p.types.members(of: u)
     assert(o == false)
@@ -69,76 +77,72 @@ struct TypeLayoutCache {
       record: t,
       havingMembers: ms.map {
         (type: MonomorphicTypeIdentity($0), label: nil)
-      }
+      },
+      in: &p
     )
   }
 
-  /// Returns the layout for a record `t` having members `ms`.
+  /// Returns the layout for a record `t` in `p` having members `ms`.
   private mutating func computeLayout(
     record t: MonomorphicTypeIdentity,
-    havingMembers ms: [(type: MonomorphicTypeIdentity, label: String?)]
+    havingMembers ms: [(type: MonomorphicTypeIdentity, label: String?)],
+    in p: inout Program
   ) -> TypeLayout {
-    if ms.isEmpty {
-      return TypeLayout(
-        bytes: .init(alignment: 1, size: 0), type: t,
-        parts: [], isEnumLayout: false
-      )
-    }
-    let f = ms.first!
-    var b = self[f.type].bytes
-    var parts: [TypeLayout.Part] = [.init(name: f.label ?? "0", type: f.type, offset: 0)]
-    for (i, p) in ms.enumerated().dropFirst() {
-      let c = self[p.type].bytes
+    var b = TypeLayout.Bytes(alignment: 1, size: 0)
+    var parts: [TypeLayout.Part] = []
+    for (i, m) in ms.enumerated() {
+      let c = layout(m.type, in: &p).bytes
       b = b.appending(c)
       parts.append(
-        .init(
-          name: p.label ?? String(describing: i + 1),
-          type: p.type, offset: b.size - c.size))
+        .init(name: m.label ?? String(i), type: m.type, offset: b.size - c.size))
     }
     return TypeLayout(bytes: b, type: t, parts: parts, isEnumLayout: false)
   }
 
-  /// Returns true iff `t` is of `MachineType`.
-  private func isMachineType(_ t: AnyTypeIdentity) -> Bool {
-    tag(t) == MachineType.self
+  /// Returns true iff `t` in `p` is of `MachineType`.
+  private func isMachineType(_ t: AnyTypeIdentity, in p: Program) -> Bool {
+    tag(t, in: p) == MachineType.self
   }
 
-  /// Returns true iff `t` is of enum type.
-  private func isEnum(_ t: AnyTypeIdentity) -> Bool {
-    let u = tag(t)
+  /// Returns true iff `t` in `p` is of enum type.
+  private func isEnum(_ t: AnyTypeIdentity, in p: Program) -> Bool {
+    let u = tag(t, in: p)
     if u == Enum.self {
       return true
     } else if u == TypeApplication.self {
-      return isEnum(type(t, as: TypeApplication.self).abstraction)
+      return isEnum(type(t, as: TypeApplication.self, in: p).abstraction, in: p)
     } else {
       return false
     }
   }
 
-  /// Returns true iff `t` is a `Struct`.
-  private func isStruct(_ t: AnyTypeIdentity) -> Bool {
-    let u = tag(t)
+  /// Returns true iff `t` in `p` is a `Struct`.
+  private func isStruct(_ t: AnyTypeIdentity, in p: Program) -> Bool {
+    let u = tag(t, in: p)
     if u == Struct.self {
       return true
     } else if u == TypeApplication.self {
-      return isStruct(type(t, as: TypeApplication.self).abstraction)
+      return isStruct(type(t, as: TypeApplication.self, in: p).abstraction, in: p)
     } else {
       return false
     }
   }
 
-  /// Returns true iff `t` is a `Tuple`.
-  private func isTuple(_ t: AnyTypeIdentity) -> Bool {
-    tag(t) == Tuple.self
+  /// Returns true iff `t` in `p` is a `Tuple`.
+  private func isTuple(_ t: AnyTypeIdentity, in p: Program) -> Bool {
+    tag(t, in: p) == Tuple.self
   }
 
-  /// Returns the type identified by `t`, cast to `U`.
-  private func type<U: TypeTree>(_ t: AnyTypeIdentity, as u: U.Type) -> U {
+  /// Returns the type identified by `t` in `p`, cast to `U`.
+  private func type<U: TypeTree>(
+    _ t: AnyTypeIdentity, as u: U.Type,
+    in p: Program
+  ) -> U {
     p.types[p.types.cast(t, to: u)!]
   }
 
-  /// Returns the tag of `t`.
-  private func tag(_ t: AnyTypeIdentity) -> any TypeTree.Type {
+  /// Returns the tag of `t` in `p`.
+  private func tag(_ t: AnyTypeIdentity, in p: Program) -> any TypeTree.Type {
     p.types.tag(of: t).value
   }
 }
