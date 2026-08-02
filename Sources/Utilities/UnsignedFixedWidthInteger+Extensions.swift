692279f11cf76a70fb7152c70439f6476f504995
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2026-08-02 19:05:47 +0530

    get rid of bytesToBits and bitsToBytes

--- Sources/Utilities/UnsignedFixedWidthInteger+Extensions.swift
+++ Sources/Utilities/UnsignedFixedWidthInteger+Extensions.swift
@@ -1,18 +1,8 @@
 extension FixedWidthInteger where Self: UnsignedInteger {
 
-  /// The number of bytes needed to hold `self` bits.
-  public var bitsToBytes: UInt {
-    (UInt(self) + 7) / 8
-  }
-
-  /// The number of bits in `self` bytes.
-  public var bytesToBits: UInt {
-    UInt(self) * 8
-  }
-
   /// The number of bits required to represent `self`.
   public var bitsInRepresentation: UInt {
-    UInt(MemoryLayout.size(ofValue: self)).bytesToBits - UInt(self.leadingZeroBitCount)
+    UInt(MemoryLayout.size(ofValue: self)) * 8 - UInt(self.leadingZeroBitCount)
   }
 
   /// Self, rounded up to the nearest power of 2.
