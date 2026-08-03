6bc747d262c0090104d0c30ca775ee4159d5037f
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2026-08-03 17:52:50 +0530

    test allocations

--- Tests/InterpreterTests/Utilities.swift
+++ Tests/InterpreterTests/Utilities.swift
@@ -2,6 +2,7 @@ import Driver
 import Foundation
 import FrontEnd
 import Interpreter
+import XCTest
 
 extension Program {
 
@@ -27,4 +28,14 @@ extension Program {
     }
   }
 
+}
+
+/// Executes `action` and reports test failure if it does not throw `error`.
+public func check<E: Error & Equatable, R>(
+  throws expectedError: E, _ action: () throws -> R, file: StaticString = #filePath,
+  line: UInt = #line
+) {
+  XCTAssertThrowsError(try action(), file: file, line: line) {
+    XCTAssertEqual($0 as? E, expectedError, "\($0)", file: file, line: line)
+  }
 }
