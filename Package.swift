94ec33febfe457be405bf51fb8b035a33f34aba9
Rishabh Dwivedi <32463042+tothambrus11@users.noreply.github.com>
2026-08-08 19:04:28 +0530

    Interpreter tests using driver (#270)
    * use driver for invoking compilation pipeline
    
    * remove the use of tree printer
    
    * Program to have an interpret extension
    
    * push the missed file :(
    
    * import docs
    
    * fix call to loadForInterpretation
    
    * loading stdlib should also enable standard library dependency
    
    * guard for module not be standard library
    
    * use noStandardLibary
    
    * remove custom printing
    
    * remove additional line
    
    * fix comments by dave
    
    * fix line length check
    
    * make the order of sources identical
    
    ---------
    
    Co-authored-by: Ambrus Toth <ping@ambrus.dev>

--- Package.swift
+++ Package.swift
@@ -185,7 +185,7 @@ let package = Package(
     .testTarget(
       name: "InterpreterTests",
       dependencies: [
-        "Interpreter", "FrontEnd",
+        "Interpreter", "FrontEnd", "Driver"
       ],
       resources: [
         .copy("InterpreterTestPrograms")
