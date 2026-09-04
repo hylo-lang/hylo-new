5df581510b3585a1a0a40d50eab298e1c3c8c931
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2026-09-04 10:21:33 +0530

    Interpreter: remove machine independent details from TargetABI (#532)
    * target abi to only allow alignment customization
    
    * explicit function for rounding up division
    
    * precondition checks for dividedRoundingUp
    
    * size -> bitsInAPointer
    
    * fix failing tests
    
    * apply doc suggestion by dave
    
    * document alignment guarantee of UnrealABI
    
    * rename roundingUp(afterDividingBy) as suggested from Dave

--- Sources/Interpreter/TargetABI.swift
+++ Sources/Interpreter/TargetABI.swift
@@ -3,12 +3,40 @@ import FrontEnd
 /// Types that describe the ABI for which we might interpret code.
 protocol TargetABI {
 
+  /// Returns the alignment of `t`, which is 1 for `.i(1)` and `.i(8)`.
+  func alignment(_ t: MachineType) -> Int
+
+  /// The number of bits in a pointer type.
+  var bitsInAPointer: Int { get }
+
+}
+
+extension TargetABI {
+
+  /// Returns the size, in bytes, of `t` on `self`.
+  public func size(_ t: MachineType) -> Int {
+    switch t {
+    case .i(let w): Int(w.roundingUp(afterDividingBy: 8))
+    case .word: bitsInAPointer / 8
+    case .float16: 2
+    case .float32: 4
+    case .float64: 8
+    case .float128: 16
+    case .ptr: bitsInAPointer / 8
+    }
+  }
+
   /// Returns the size and alignment of `t` instances.
-  func footprint(_ t: MachineType) -> TypeLayout.StorageRequirements
+  public func footprint(_ t: MachineType) -> TypeLayout.StorageRequirements {
+    .init(alignment: alignment(t), size: size(t))
+  }
 
 }
 
 /// An ABI we can use to interpret code when matching some real ABI doesn't matter.
+///
+/// The alignment is the size of `t`, capped at `maxAlignment`. In particular,
+/// `.i(1)` and `.i(8)` have one-byte alignment.
 struct UnrealABI: TargetABI {
 
   /// An instance.
@@ -20,33 +48,11 @@ struct UnrealABI: TargetABI {
   /// The maximal alignment of a machine type in bytes.
   private let maxAlignment = 128 / 8
 
-  /// Returns the size and alignment of `bitWidth`-bit machine types.
-  ///
-  /// - Precondition: `bitWidth` is a power of 2.
-  private func machineTypeFootprint(bitWidth: Int) -> TypeLayout.StorageRequirements {
-    precondition(
-      bitWidth > 0 && bitWidth.nonzeroBitCount == 1,
-      "bit width \(bitWidth) is not a power of 2.")
-    let sizeInBytes = (bitWidth + 7) / 8
-    return .init(
-      alignment: min(sizeInBytes, maxAlignment),
-      size: sizeInBytes)
-  }
+  /// Returns the alignment of `t`, which is 1 for `.i(1)` and `.i(8)`.
+  func alignment(_ t: MachineType) -> Int { min(size(t), maxAlignment) }
 
-  /// Returns the size and alignment of `t` instances.
-  public func footprint(_ t: MachineType) -> TypeLayout.StorageRequirements {
-    let bitWidth =
-      switch t {
-      case .i(let w): Int(w)
-      case .word: bitsInAWord
-      case .float16: 16
-      case .float32: 32
-      case .float64: 64
-      case .float128: 128
-      case .ptr: bitsInAWord
-      }
-    return machineTypeFootprint(bitWidth: bitWidth)
-  }
+  /// The number of bits in a pointer type.
+  var bitsInAPointer: Int { bitsInAWord }
 
 }
 
