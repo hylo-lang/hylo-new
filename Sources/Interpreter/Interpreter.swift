dc9c882cd3419f65c8853a7ef4d054ffacb46aa8
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2026-09-07 16:07:16 +0530

    better doc for result

--- Sources/Interpreter/Interpreter.swift
+++ Sources/Interpreter/Interpreter.swift
@@ -84,7 +84,7 @@ private struct Value {
 /// to another instruction.
 private enum InstructionEpilogue {
 
-  /// Store the given value to instruction register.
+  /// Initialize instruction register with the given value.
   case result(Value)
 
   /// Control is transferred to the given instruction.
